44cc23e70a2372fffec80fba564c6ed9578bb50c
Vladislav Alpeev <vlad.alpeev@mail.ru>
2021-06-25 01:28:11 +0300

    Внедрено использование API HH.ru

--- hhru_api/hhru_api/Controllers/ApiUniversitiesController.cs
+++ hhru_api/hhru_api/Controllers/ApiUniversitiesController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace hhru_api.Controllers
 {
@@ -14,15 +18,69 @@ namespace hhru_api.Controllers
 
         private readonly ILogger<ApiUniversitiesController> _logger;
 
+        private static ProductInfoHeaderValue productValue = new ProductInfoHeaderValue("UniversityAPI", "1.0");
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private static string baseURL = "https://api.hh.ru/";
+
+        private University university;
+        private List<Faculty> faculties;
+
+
         public ApiUniversitiesController(ILogger<ApiUniversitiesController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public String PostUniversity(String identifier)
+        public void PostUniversity(string identifier)
         {
-            return identifier;
+            if (identifier == null)
+                return;
+
+            if (!getUniversityData(identifier))
+                Response.StatusCode = 400;
+            else
+                Response.StatusCode = 200;
+
+            return;
+        }
+
+        private bool getUniversityData(string ID)
+        {
+            client.DefaultRequestHeaders.UserAgent.Add(productValue);
+            Uri uri;
+            string responseBody;
+
+            try
+            {
+                uri = new Uri(baseURL + "educational_institutions/?id=" + ID);
+                var response = client.GetAsync(uri).Result;
+                responseBody = response.Content.ReadAsStringAsync().Result;
+
+                Universities universities = JsonConvert.DeserializeObject<Universities>(responseBody);
+                university = universities.getFirst();
+            }
+            catch
+            {
+                return false;
+            }
+
+            try
+            {
+                uri = new Uri(baseURL + "educational_institutions/" + ID + "/faculties");
+                var response = client.GetAsync(uri).Result;
+                responseBody = response.Content.ReadAsStringAsync().Result;
+
+                faculties = JsonConvert.DeserializeObject<List<Faculty>>(responseBody);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
