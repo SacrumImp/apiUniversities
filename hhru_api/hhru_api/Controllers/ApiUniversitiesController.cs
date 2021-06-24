108eb09a924f44ec0c3d386e5b27c20401989c19
Vladislav Alpeev <vlad.alpeev@mail.ru>
2021-06-24 17:39:28 +0300

    Инициализация проекта, добавление файла .gitignore.

---
+++ hhru_api/hhru_api/Controllers/ApiUniversitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hhru_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiUniversitiesController : ControllerBase
+    {
+
+        private readonly ILogger<ApiUniversitiesController> _logger;
+
+        public ApiUniversitiesController(ILogger<ApiUniversitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String PostUniversity(String identifier)
+        {
+            return identifier;
+        }
+    }
+}
