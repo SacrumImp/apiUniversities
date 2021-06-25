9f423b6c25c56ca2c31f8e39f605ff00ab5c90e5
Vladislav Alpeev <vlad.alpeev@mail.ru>
2021-06-25 22:21:37 +0300

    Настроено соединение с базой данных MS SQL.

--- hhru_api/hhru_api/Startup.cs
+++ hhru_api/hhru_api/Startup.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using hhru_api.Models;
 
 namespace hhru_api
 {
@@ -25,6 +23,8 @@ namespace hhru_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UniversitiesEntities")));
+
             services.AddControllers();
         }
 
